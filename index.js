2b7bccf37d332de5152eb645c9b1b9fd18ef0ec9
Luiz Fernando Vital <luizfvital@gmail.com>
2024-08-24 11:19:13 -0300

    feat: create Timer class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi');
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start() {
+    console.log('start the timer!')
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+
+console.log(timer);
